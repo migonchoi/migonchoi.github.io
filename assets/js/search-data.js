60c634229939780df1a27e635e2e3a4ddf2ff460
migonchoi <122828021+migonchoi@users.noreply.github.com>
2026-03-11 23:51:28 +0000

    Deploying to gh-pages from @ migonchoi/migonchoi.github.io@3ec1d7df5a3e602f525535ba36d75206f6996f9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "AI Scientist focused on decision-centric machine learning for scientific discovery.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
