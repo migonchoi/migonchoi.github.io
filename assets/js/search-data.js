6f8d19738d896f6928b79c1a48f83504295453cd
migonchoi <122828021+migonchoi@users.noreply.github.com>
2026-09-18 15:48:53 +0000

    Deploying to gh-pages from @ migonchoi/migonchoi.github.io@ea302224593c7cc955305a11631b8fde874e4a8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
